e1498ecc893540c11d2b0de92fe4d27acc46c23c
moroZ-dev <casha.morozov.2015@mail.ru>
2025-03-06 18:51:10 +0300

    Try using git + VS

--- Rock_Paper_Scissor/Rock_Paper_Scissor/Random.cpp
+++ Rock_Paper_Scissor/Rock_Paper_Scissor/Random.cpp
@@ -6,10 +6,10 @@ int randomChoose()
 	srand(static_cast<unsigned int>(time(0)));
 	static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
 	// Равномерно распределяем рандомное число в нашем диапазоне
-	int x;
+	int y;
 	for (int i = 0; i < 10; i++)
 	{
-		x = static_cast<int>(rand() * fraction * (3 - 1 + 1) + 1);
+		y = static_cast<int>(rand() * fraction * (3 - 1 + 1) + 1);
 	}
-	return x;
+	return y;
 }
